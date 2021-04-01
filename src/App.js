b053f696d0fe49df92edd6b9bfe5d970166aeeb9
Pablo Guerra <pabloguerra2006@yahoo.com>
2021-04-01 11:46:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState, useEffect } from "react";
+import axios from "axios";
+import Table from "./Table";
+import "./App.css";
+
+// Custom component to render Genres
+const Genres = ({ values }) => {
+    // Loop through the array and create a badge-like component instead of a comma-separated string
+  return (
+    <>
+      {values.map((genre, idx) => {
+        return (
+          <span key={idx} className="badge">
+            {genre}
+          </span>
+        );
+      })}
+    </>
+  );
+};
 
 function App() {
+
+  /* 
+    - Columns is a simple array right now, but it will contain some logic later on. 
+      It is recommended by react-table to memoize the columns data.
+    - Here in this example, we have grouped our columns into two headers.
+      react-table is flexible enough to create grouped table headers.
+  */
+ const columns = useMemo(
+  () => [
+    {
+      // first group - TV Show
+      Header: "TV Show",
+      // First group columns
+      columns: [
+        {
+          Header: "Name",
+          accessor: "show.name"
+        },
+        {
+          Header: "Type",
+          accessor: "show.type"
+        }
+      ]
+    },
+    {
+      // Second group - Details
+      Header: "Details",
+      // Second group columns
+      columns: [
+        {
+          Header: "Language",
+          accessor: "show.language"
+        },
+        {
+          Header: "Genre(s)",
+          accessor: "show.genres",
+          // Cell method will provide the cell value; we pass it to render a custom component
+          Cell: ({ cell: { value } }) => <Genres values={value} />
+        },
+        {
+          Header: "Runtime",
+          accessor: "show.runtime",
+          // Cell method will provide the value of the cell; we can create a custom element for the Cell        
+          Cell: ({ cell: { value } }) => {
+            const hour = Math.floor(value / 60);
+            const min = Math.floor(value % 60);
+            return (
+              <>
+                {hour > 0 ? `${hour} hr${hour > 1 ? "s" : ""} ` : ""}
+                {min > 0 ? `${min} min${min > 1 ? "s" : ""}` : ""}
+              </>
+            );
+          }
+        },
+        {
+          Header: "Status",
+          accessor: "show.status"
+        }
+      ]
+    }
+  ],
+  []
+);
+
+
+
+
+  // API endpoint usage.
+  // data state to store the TV Maze API data. Its initial value is an empty array
+  const [data, setData] = useState([]);
+
+  // Using useEffect to call the API once mounted and set the data
+  useEffect(() => {
+    (async () => {
+      const result = await axios("https://api.tvmaze.com/search/shows?q=snow");
+      setData(result.data);
+    })();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table columns={columns} data={data} />
     </div>
   );
 }
